1ac99a8025e7aba5b2fa94edd50649a7fd6ba50d
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-20 06:14:00 +0100

    new update in client interface

--- project/version2/src/main/java/com/mutuelle/project/interfaces/ClientInterface.java
+++ project/version2/src/main/java/com/mutuelle/project/interfaces/ClientInterface.java
@@ -3,11 +3,11 @@ package com.mutuelle.project.interfaces;
 import com.mutuelle.project.model.Client;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface ClientInterface {
-    public void addClientInFile(Client c);
-    public void filterClient(String number);
-    public ArrayList<Client> readClients();
-
+    public Client getClient();
+    public List<Client> getListClients();
+    public Client addClient();
 
 }
