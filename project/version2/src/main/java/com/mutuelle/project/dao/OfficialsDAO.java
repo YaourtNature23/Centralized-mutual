4435365c78599c8efbc703e3744fa1f061766e89
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-20 16:36:55 +0100

    add officialDAO

---
+++ project/version2/src/main/java/com/mutuelle/project/dao/OfficialsDAO.java
@@ -0,0 +1,70 @@
+package com.mutuelle.project.dao;
+
+
+import com.mutuelle.project.controller.PanelController;
+import com.mutuelle.project.model.Entity;
+import com.mutuelle.project.model.Officials;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class OfficialsDAO extends DAO<Officials>{
+
+    public Officials getOfficialByEmailAndPassword(String email,String password){
+         String verifyLogin = " SELECT * FROM officials inner join entity on officials.ent_id = entity.ent_id WHERE email  ='"+email+"' and password='"+password+"' LIMIT 1;";
+            try{
+                Statement statement = connectDB.createStatement();
+                ResultSet queryResult = statement.executeQuery(verifyLogin);
+                while (queryResult.next()) {
+                    if (queryResult.getInt("official_id") > 0) {
+                        PanelController.id = queryResult.getInt("official_id");
+                       return new Officials(queryResult.getInt("official_id"), queryResult.getString("firstname"), queryResult.getString("lastname"), queryResult.getString("cin"), queryResult.getString("phone"), queryResult.getString("email"), queryResult.getString("password"), new Entity(queryResult.getInt("ent_id"),queryResult.getString("ent_name"),queryResult.getString("ent_addresse"),queryResult.getString("ent_phone"),queryResult.getString("ent_site")));
+                    } else {
+                        System.out.println("Invalid Login, try again dao");
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                e.getCause();
+            }
+        return null;
+    }
+    @Override
+    public Officials find(int id) {
+        String verifyLogin = " SELECT * FROM officials inner join entity on officials.ent_id = entity.ent_id WHERE official_id ="+id+" LIMIT 1";
+        try{
+            Statement statement = connectDB.createStatement();
+            ResultSet queryResult = statement.executeQuery(verifyLogin);
+            while (queryResult.next()) {
+
+                if (queryResult.getRow() > 0) {
+                    PanelController.id = queryResult.getInt("official_id");
+                    return new Officials(queryResult.getInt("official_id"), queryResult.getString("firstname"), queryResult.getString("lastname"), queryResult.getString("cin"), queryResult.getString("phone"), queryResult.getString("email"), queryResult.getString("password"), new Entity(queryResult.getInt("ent_id"),queryResult.getString("ent_name"),queryResult.getString("ent_addresse"),queryResult.getString("ent_phone"),queryResult.getString("ent_site")));
+                } else {
+                    System.out.println("Invalid Login, try again dao");
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+        return null;
+    }
+
+
+    @Override
+    public Officials create(Officials obj) {
+        return null;
+    }
+
+    @Override
+    public Officials update(Officials obj) {
+        return null;
+    }
+
+    @Override
+    public void delete(Officials obj) {
+
+    }
+
+}
