f6168a337d10f527d88f4059bd2550f743798082
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-20 16:39:03 +0100

    add factoryDAO

---
+++ project/version2/src/main/java/com/mutuelle/project/implement/FactoryDAO.java
@@ -0,0 +1,55 @@
+package com.mutuelle.project.implement;
+
+
+import com.mutuelle.project.dao.ClientDAO;
+import com.mutuelle.project.dao.OfficialsDAO;
+import com.mutuelle.project.model.Client;
+import com.mutuelle.project.model.Officials;
+import javafx.scene.chart.XYChart;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class FactoryDAO {
+
+
+    public static Officials getOfficialByEmailAndPassword(String Email, String Password) {
+        return new OfficialsDAO().getOfficialByEmailAndPassword(Email, Password);
+    }
+
+    public static Officials getOfficialById(int id) {
+        return new OfficialsDAO().find(id);
+    }
+
+
+    public static List<Client> getListClientForOfficial(int id) {
+
+        return new ClientDAO().getClientByIdOfficial(id);
+
+    }
+
+    public static List<Client> filterClient(String badge, String fName, String lName, String cin, String email) {
+        return new ClientDAO().filterClient(badge, fName, lName, cin, email);
+    }
+
+
+    public static Client insertClient(Client client) {
+
+        return new ClientDAO().create(client);
+
+    }
+
+    public static XYChart.Series getStatisticMonth() {
+        return new ClientDAO().getStaticMonthStartWork();
+    }
+
+    public static XYChart.Series getStatisticDayCreated() {
+        return new ClientDAO().getStaticDaysCreated();
+    }
+
+    public static XYChart.Series getStatisticYearCreated() {
+        return new ClientDAO().getStaticYearCreated();
+    }
+
+
+}
