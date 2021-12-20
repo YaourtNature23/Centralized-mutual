8dc6d0d805335c5f3b39b45f83e0919a24c8bc3e
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-12-20 16:35:42 +0100

    EntityDAO

---
+++ project/version2/src/main/java/com/mutuelle/project/dao/EntityDAO.java
@@ -0,0 +1,26 @@
+package com.mutuelle.project.dao;
+
+import com.mutuelle.project.model.Entity;
+
+public class EntityDAO extends DAO<Entity> {
+
+    @Override
+    public Entity find(int id) {
+        return null;
+    }
+
+    @Override
+    public Entity create(Entity obj) {
+        return null;
+    }
+
+    @Override
+    public Entity update(Entity obj) {
+        return null;
+    }
+
+    @Override
+    public void delete(Entity obj) {
+
+    }
+}
